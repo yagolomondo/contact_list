d0fb8f1186a8d2407ccf893a4c7b5ae0d53d236c
yagolomondo <yagolcs@msn.com>
2024-08-19 19:52:11 -0300

    Contatos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  private
+    def required_logged_in_user
+      unless required_logged_in_user
+        flash[:danger]='Area restrita. Por favor, realize o login'
+        redirect_to entrar_path
+      end
+    end
 end
